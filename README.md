698701d7590d718fead6dfd1e460fc9f8efe63d2
taoshumin_vendor <tsmidx@gmail.com>
2023-07-01 17:23:00 +0800

    feat: 添加获取accesstoken管理池子

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # ChatGPT-AccessToken
 
-简介：动态获取ChatGPT的Access Token。
+简介：主要用于批量动态生成 ChatGPT的Access Token，支持集群大批量生成过期时间校验。
 
 
 ### 项目产生的原因
 
-由于潘多拉项目生产的Access Token 不稳定。会经常出现 `An error occurred: Error request login url.`，所以想做一个临时管理Access Token的项目.
+由于潘多拉项目生产的Access Token 不稳定。会经常出现 `An error occurred: Error request login url.`。
 
+### 功能
+1. 指定邮箱只能在指定的代理IP申请。
+2. 每个代理IP申请生产access_token后休息5-10秒
+3. 加载代理池.
 
+### 打包
+
+```shell
+make snapshots
+```
---
+++ cmd/launcher/launcher.go
@@ -0,0 +1,95 @@
+/*
+Copyright 2022 The deepauto-io LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package launcher
+
+import (
+	"context"
+	"fmt"
+	"github.com/chatgpt-accesstoken/build"
+	"strings"
+	"sync"
+
+	"github.com/workpieces/log"
+)
+
+type labeledCloser struct {
+	label  string
+	closer func(context.Context) error
+}
+
+// Launcher represents the main program execution.
+type Launcher struct {
+	wg       sync.WaitGroup
+	cancel   func()
+	doneChan <-chan struct{}
+	closers  []labeledCloser
+
+	httpPort int
+	logger   log.Logger
+}
+
+// NewLauncher returns a new instance of Launcher with a no-op logger.
+func NewLauncher() *Launcher {
+	return &Launcher{
+		logger: log.NewNop(),
+	}
+}
+
+// Shutdown shuts down the HTTP server and waits for all services to clean up.
+func (m *Launcher) Shutdown(ctx context.Context) error {
+	var errs []string
+
+	// Shut down subsystems in the reverse order of their registration.
+	for i := len(m.closers); i > 0; i-- {
+		lc := m.closers[i-1]
+		m.logger.
+			WithField("subsystem", lc.label).
+			Info("stopping subsystem")
+		if err := lc.closer(ctx); err != nil {
+			m.logger.
+				WithField("subsystem", lc.label).
+				Error("Failed to stop subsystem: ", err)
+			errs = append(errs, err.Error())
+		}
+	}
+
+	m.wg.Wait()
+
+	if len(errs) > 0 {
+		return fmt.Errorf("failed to shut down server: [%s]", strings.Join(errs, ","))
+	}
+	return nil
+}
+
+func (m *Launcher) Done() <-chan struct{} {
+	return m.doneChan
+}
+
+func (m *Launcher) run(ctx context.Context, opts Config) error {
+	ctx, m.cancel = context.WithCancel(ctx)
+	m.doneChan = ctx.Done()
+
+	info := build.GetBuildInfo()
+
+	m.logger.
+		WithField("version", info.Version).
+		WithField("commit", info.Commit).
+		WithField("build_date", info.Date).
+		Info("welcome to akt")
+
+	return nil
+}
